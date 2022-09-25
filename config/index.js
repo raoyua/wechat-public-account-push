00c87a37b0adfb4ddd6bd7fea3090703c3434535
raoyua <114336059+raoyua@users.noreply.github.com>
2022-09-25 18:23:34 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -82,14 +82,14 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '老婆', year: '1996', date: '09-09',
+          type: '*生日', name: '鸣宝', year: '2002', date: '05-01',
         },
         {
-          type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '恋爱时长', year: '2022', date: '04-25',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '小杨bb', year: '2002', date: '02-24',
         },
         {
           type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
