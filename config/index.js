85d4deb355c9c5d5b20d6aa01ca0df4eae30f518
raoyua <114336059+raoyua@users.noreply.github.com>
2022-09-25 19:12:31 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oserH5seu2FYroFXQ_sKsK6wcbpE',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'zjx5bp7FpbX8-ot5xKF6QR4bReqn9U-Z05flGSh73os',
+      useTemplateId: 'EKU2whJpVWPXvIbzUILgvySR8L319uCChNujg2tyWqo',
       // 所在省份
       province: '山东',
       // 所在城市
